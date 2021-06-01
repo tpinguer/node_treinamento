805d45decb6b70b128bc7f8cbd6de35bb2233376
Thiago <tpinguermello@gmail.com>
2021-06-01 11:52:28 -0300

    correção na subtração do ex 01

--- exercicios/01-exercicio/calculador.js
+++ exercicios/01-exercicio/calculador.js
@@ -16,8 +16,8 @@ const calculadora = {
         return a - b;
     },
     subtracaoNnumeros(...numeros){
-        let resultado = 0;
-        for(let i = 0; i < numeros.length; i++){
+        let resultado = numeros[0];
+        for(let i = 1; i < numeros.length; i++){
             resultado = resultado - numeros[i];
         }
         return resultado;
